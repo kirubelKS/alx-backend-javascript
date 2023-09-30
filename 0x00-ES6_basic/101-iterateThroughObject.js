b4531ec5f7066238a27fc319cd5ae1d22445db65
kirubelks <117734684+kirubelKS@users.noreply.github.com>
2023-09-30 05:36:31 +0300

    edit 101 check 1

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-    const employeeNames = [...reportWithIterator].join('|');
-    return employeeNames;
-  }
-  
+  return reportWithIterator.join(' | ');
+}
